bb9a9a027e8506969f6c2ed38d8863d048aca074
Saba <saba.sadegh67@gmail.com>
2022-12-31 20:34:15 +0330

    declare variable and define initial functions

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('wiki starter');
+const url =
+  "https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch=searchValue";
+
+const formDOM = document.querySelector(".form");
+const inputDOM = document.querySelector(".form-input");
+const resultsDOM = document.querySelector(".results");
+
+formDOM.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const value = inputDOM.value;
+  if (!value) {
+    resultsDOM.innerHTML = `<div class="error">please enter valid search term</div>`;
+    return;
+  }
+  fetchPages(value);
+});
+
+const fetchPages = async (searchValue) => {};
